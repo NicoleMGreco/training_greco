0e1e4c8deec2031fcc934eb38bdd205dfd87e739
NicoleMGreco <nicolegreco@ufl.edu>
2024-10-08 10:22:18 -0700

    Initial commit

---
+++ scripts/intro-to-r.R
@@ -0,0 +1,42 @@
+important_value<- 3*4
+weight_kg<- 25
+2.2*weight_kg
+
+# creating a vector
+weight_kg<- c(25,33,12)
+weight_kg
+weight_kg*2.2
+
+# save function as object
+weight_lb<- weight_kg*2.2
+
+science_rocks<-"yes it does!"
+class(science_rocks)
+class(important_value)
+
+is.numeric(science_rocks)
+
+# vector examples
+
+# character vector
+chr_vector<-c("hello","goodbye","see you later")
+
+# numeric vector
+num_vector<-c(5,1.3,10)
+
+# logical vector
+boolean_vector<-c(TRUE,FALSE,FALSE)
+
+# using the mean() function
+weight_lb<-mean(x=weight_lb)
+weight_lb<-c(60,30,17)
+
+weight_lb_mean<-mean(weight_lb)
+
+# reading data with read.csv()
+bg_chem_dat<- read.csv(file="data/BGchem2008data.csv")
+head(bg_chem_dat)
+head(bg_chem_dat$Date)
+mean(bg_chem_dat$CTD_Temperature)
+
+
